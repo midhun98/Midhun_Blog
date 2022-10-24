585a37c519ebc7dc0efaf7ad7dcf4dd6e270bb71
midhun98 <midhunskani@gmail.com>
2022-10-24 20:26:56 +0530

    Profile page

---
+++ core/urls.py
@@ -0,0 +1,9 @@
+from django.urls import include, path
+from . import views
+from django.contrib import admin
+from django.contrib.auth.decorators import permission_required
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("", views.BASE, name='base'),
+]
