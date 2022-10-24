8186a20537648c4ff277eadf4df5fa09c772647e
midhun98 <midhunskani@gmail.com>
2022-10-25 00:48:33 +0530

    Profile page changes

--- core/views.py
+++ core/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+import requests
 # Create your views here.
-def BASE(request):
-    if request.method == "GET":
-        return render(request, 'details.html')
+
+class ProfilePage(APIView):
+    def get(self, request):
+        if request.method == "GET":
+            context = {}
+            r = requests.get('https://gh-pinned-repos.egoist.dev/?username=midhun98')
+            r_status = r.status_code
+            if r_status == 200:
+                data = r.json()
+                context['pinned_repo']= data
+                print(context)
+            return render(request, 'details.html', context)
