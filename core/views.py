30ef7d2e8addd40bc1bdc1a7e05395b9bde9d6e9
midhun98 <midhunskani@gmail.com>
2022-10-26 19:07:01 +0530

    Contact me page send email

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import generic
 from rest_framework.views import APIView
 import requests
+from . import forms
+from django.core.mail import send_mail
+from django.contrib import messages
 # Create your views here.
 
 class ProfilePage(APIView):
@@ -12,7 +15,7 @@ class ProfilePage(APIView):
             r_status = r.status_code
             if r_status == 200:
                 data = r.json()
-                context['pinned_repo']= data
+                context['pinned_repo'] = data
             return render(request, 'details.html', context)
 
 class ResumePage(generic.TemplateView):
@@ -21,8 +24,33 @@ class ResumePage(generic.TemplateView):
             context = {}
             return render(request, 'resume.html', context)
 
+
 class ContactsPage(generic.TemplateView):
     def get(self, request, *args, **kwargs):
         if request.method == "GET":
             context = {}
+            form = forms.ContactForm()
+            context['communication_form'] = form
+            return render(request, 'contacts.html', context)
+
+    def post(self, request, *args, **kwargs):
+        if request.method == 'POST':
+            context = {}
+            form = forms.ContactForm(request.POST)
+            if form.is_valid():
+                name = form.data['name']
+                email = form.data['email']
+                subject = form.data['subject']
+                content = form.data['content']
+                print(name)
+                print(email)
+                print(subject)
+                print(content)
+                send_mail(subject, subject, email, ['midhunskani@gmail.com'])
+                return redirect('contacts_page')
+            else:
+                messages.warning(request, 'Please enter valid details.')
+                form = forms.ContactForm()
+                context['communication_form'] = form
+
             return render(request, 'contacts.html', context)
