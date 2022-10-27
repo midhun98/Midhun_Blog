167c8a83d795a8e52482923b3db9ef9075085775
midhun98 <midhunskani@gmail.com>
2022-10-27 18:28:02 +0530

    Added blog page

--- core/urls.py
+++ core/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path("", views.ProfilePage.as_view(), name='profile_page'),
     path("resume/", views.ResumePage.as_view(), name='resume_page'),
     path("contacts/", views.ContactsPage.as_view(), name='contacts_page'),
+    path("blog/", views.BlogPage.as_view(), name='blog_page'),
 ]
