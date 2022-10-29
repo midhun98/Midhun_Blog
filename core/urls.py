fa8053f2eb6102dbb8dfdfb5febbf97211490619
midhun98 <midhunskani@gmail.com>
2022-10-29 14:16:43 +0530

    Added permissions for creating blog

--- core/urls.py
+++ core/urls.py
@@ -2,13 +2,14 @@ from django.urls import include, path
 from . import views
 from django.contrib import admin
 from django.contrib.auth.decorators import permission_required
+from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.ProfilePage.as_view(), name='profile_page'),
     path("resume/", views.ResumePage.as_view(), name='resume_page'),
     path("contacts/", views.ContactsPage.as_view(), name='contacts_page'),
-    path("blog/", views.BlogPage.as_view(), name='blog_page'),
+    path("blog/", login_required(views.BlogPage.as_view()), name='blog_page'),
     path("login/", views.login_request, name='login'),
     path("user_create/", views.UserCreateView.as_view(), name='user_create'),
     path("blog-list/", views.BlogListView.as_view(), name='blog_list'),
