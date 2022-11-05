ac1cad074b5ab483a4ce6ef422e49098009ccd55
midhun98 <midhunskani@gmail.com>
2022-11-05 12:16:54 +0530

    minor changes

--- core/views.py
+++ core/views.py
@@ -18,14 +18,13 @@ from django.urls import reverse
 
 class ProfilePage(APIView):
     def get(self, request):
-        if request.method == "GET":
-            context = {}
-            r = requests.get('https://gh-pinned-repos.egoist.dev/?username=midhun98')
-            r_status = r.status_code
-            if r_status == 200:
-                data = r.json()
-                context['pinned_repo'] = data
-            return render(request, 'details.html', context)
+        context = {}
+        r = requests.get('https://gh-pinned-repos.egoist.dev/?username=midhun98')
+        r_status = r.status_code
+        if r_status == 200:
+            data = r.json()
+            context['pinned_repo'] = data
+        return render(request, 'details.html', context)
 
 
 class ResumePage(generic.TemplateView):
